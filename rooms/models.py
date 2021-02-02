5200f928cf7daa21004ec216bbcb3c0274a82c02
taeil.jang <taeil.jang@linkshops.com>
2021-02-02 22:23:46 +0900

    #4 : Room Model 구현

--- rooms/models.py
+++ rooms/models.py
@@ -3,6 +3,47 @@ from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
 
+class AbstractItem(core_models.TimeStampedModel):
+
+	"""" Abstract Item """
+
+	name = models.CharField(max_length=80)
+
+	class Meta:
+		abstract = True
+
+	def __str__(self):
+		return self.name
+
+class RoomType(AbstractItem):
+
+	""" RoomType object Definition """
+
+	class Meta:
+		verbose_name = "Room Type"
+		ordering = ['name']
+
+class Amenity(AbstractItem):
+
+	""" Amenity object Definition """
+
+	class Meta:
+		verbose_name_plural = "Amenities"
+
+class Facility(AbstractItem):
+
+	""" Facility object Definition """
+
+	class Meta:
+		verbose_name_plural = "Facilities"
+
+class HouseRule(AbstractItem):
+
+	""" HouseRule object Definition """
+
+	class Meta:
+		verbose_name = "House Rule"
+
 class Room(core_models.TimeStampedModel):
 
 	""" Room Model Definition """
@@ -21,3 +62,22 @@ class Room(core_models.TimeStampedModel):
 	check_out = models.TimeField()
 	instant_book = models.BooleanField(default=False)
 	host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+	room_type = models.ForeignKey(RoomType,  on_delete=models.SET_NULL, null=True)
+	amenities = models.ManyToManyField(Amenity, blank=True)
+	facilities = models.ManyToManyField(Facility, blank=True)
+	house_rules = models.ManyToManyField(HouseRule, blank=True)
+	
+	def __str__(self):
+		return self.name
+
+
+class Photo(core_models.TimeStampedModel):
+	
+	""" Photo object Definition """
+
+	caption = models.CharField(max_length=80)
+	file = models.ImageField()
+	room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.caption
